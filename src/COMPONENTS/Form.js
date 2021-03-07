8242630708fbde929b638eb6a51914c06bb115d3
matzhd <77358430+matzhd@users.noreply.github.com>
2021-03-07 19:16:02 +0000

    start-react

---
+++ src/COMPONENTS/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Formm (){
+    return(
+        <div>
+            <form>
+                <input placeholder="enter name"/>
+                <buton>send</buton>
+            </form>
+        </div>
+
+    )
+}
+
+export default Formm
---
+++ src/COMPONENTS/Selamla.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Selam (){
+    return (
+        <div>
+            <h1>Hi React!!!</h1>
+        </div>
+    )
+}
+
+export default Selam
